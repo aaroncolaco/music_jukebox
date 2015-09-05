d0f35e3d6ed03631024f472d1167499cd23ec05d
aarongoa <taipan93@gmail.com>
2015-09-06 00:04:46 +0530

    Added a Bon Jovi video as link for all songs

--- data.rb
+++ data.rb
@@ -8,9 +8,9 @@ lyrics = "Some really cool words here..."
 
 	song.title = "Song " + number.to_s
 	song.artist = "Artist " + number.to_s
-	song.video_link = "Link " + number.to_s
+	song.video_link = "https://www.youtube.com/watch?v=nWuZMBtrc1E"
 	song.length = rand(10) + rand
-	song.released_on = rand(2015).to_s	#since it it changed to date object in song.rb
+	song.released_on = rand(2015).to_s	#since it changed to date object in song.rb
 	song.lyrics = lyrics
 	song.save
 end
