fd6fd807b6e3aa71226f29fe1dbe0340a327dba3
aarongoa <taipan93@gmail.com>
2015-09-11 22:23:01 +0530

    Added helper to set the title to the default -- appname --  if no title is specified

--- main.rb
+++ main.rb
@@ -25,6 +25,12 @@ configure :production do
 	DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 
+# Gets executed before every request. So this is done to set @title to
+# the default -- appname -- using the set_title helper
+before do
+	set_title
+end
+
 get '/' do
 	@title = "Home"
 	erb :home
@@ -147,5 +153,10 @@ end
 helpers do
 	def current?(path = '/')
 		(request.path == path || request.path == path+'/') ? "active" : nil
+	end
+	def set_title
+		# if not given, set to app name. That is why the OR
+		@title ||= settings.app_name
+		# Done so that we don't have to put this login in the erb file like before
 	end
 end
